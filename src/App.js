c37e52fbf938e71662f4c167ef4b978aaedb2e8a
Vanessa Soares <vanessa.carvalhal@detran.ba.gov.br>
2023-07-20 17:44:49 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoItem from './components/TodoItem';
+import { Container, List } from '@mui/material';
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+  const deleteTodo = (id) => {
+    var filtered = todos.filter((todo) => todo.id !== id);
+    setTodos(filtered);
+  };
+
+  const editTodo = (id, editedText) => {
+    var todosArray = todos;
+
+    for (var i in todosArray) {
+      if (todosArray[i].id == id) {
+        todosArray[i].text = editedText;
+      }
+    }
+
+
+    setTodos(todosArray);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xs" style={{ marginTop: "1em" }}>
+    <Form addTodo={addTodo} />
+    <List sx={{ marginTop: "1em" }}>
+      {todos.map((todo) => (
+        <div key={todo.id} style={{ marginTop: "1em" }}>
+          <TodoItem editTodo={editTodo} todo={todo} deleteTodo={deleteTodo} />
+        </div>
+      ))}
+    </List>
+  </Container>
   );
 }
 
